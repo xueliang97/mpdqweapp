c94d69d5d378351e0eca12e6d21041913e100afc
Xue Liang <xueliangddsb@gmail.com>
2019-04-16 20:42:52 +0800

    add authorize login in

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+import requestUrl from './utils/util.js'
 App({
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
+  onLaunch: function () {   /*
+    // 登录;用户打开小程序时，会调用wx.login获取code，将code发送到后台获取openid。后台保存opendi并返回用户信息
+    //（首次登录信息为空，非首次登录信息存在）
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          requestUrl.requestUrl({
+            url:  "" ,
+            params: {
+              code: res.code
+            },
+            method: "post",
+          })   
+            .then((data) => {//then接收一个参数，是函数，并且会拿到我们在requestUrl中调用resolve时传的的参数
+              console.log(data);//返回openId
+              this.globalData.openId = res.openId;
+            })
+            .catch((errorMsg) => {
+              console.log(errorMsg)
+            })
+        }
+      }
+    })  */
     
   },
 
@@ -26,6 +50,13 @@ App({
    */
   onError: function (msg) {
     
+  },
+
+  globalData: {
+    userInfo: "",//用户信息
+    openId: "",//登录用户的唯一标识
+    appid: 'wx242f88b4c25643c2',//appid  
+    secret: 'e0dassda466b98dd75bac9ad5b760218075b0577def2424234209bwXXXXXXXXXXXXXX',//secret秘钥
   },
 
   appData:{
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,66 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+//公共request方法
+const requestUrl = ({
+  url,
+  params,
+  success,
+  method = "post"
+}) => {
+  wx.showLoading({
+    title: '加载中',
+  });
+  let server = '';//正式域名
+  // let server = 'http://dxxx.xxxxxxxxxx.cn';//测试域名
+  let sessionId = wx.getStorageSync("sid"),
+    that = this;
+  if (sessionId != "" && sessionId != null) {
+    var header = { 'content-type': 'application/x-www-form-urlencoded', 'Cookie': 'sid=' + sessionId }
+  } else {
+    var header = { 'content-type': 'application/x-www-form-urlencoded' }
+  }
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: server + url,
+      method: method,
+      data: params,
+      header: header,
+      success: (res) => {
+        wx.hideLoading();
+        if (sessionId == "" || sessionId == null) {
+          wx.setStorageSync('sid', res.data.info.sessionId)//  如果本地没有就说明第一次请求 把返回的 sessionId 存入本地
+        }
+        if (res['statusCode'] == 200) {
+          resolve(res)//异步成功之后执行的函数
+        } else {
+          wx.showToast({
+            title: res.data.msg || '请求出错',
+            icon: 'none',
+            duration: 2000,
+            mask: true
+          })
+          reject(res.ErrorMsg);
+        }
+      },
+      fail: (res) => {
+        wx.hideLoading();
+        wx.showToast({
+          title: res.data.msg || '',
+          icon: 'none',
+          duration: 2000,
+          mask: true
+        })
+        reject('网络出错');
+      },
+      complete: function () {
+        wx.hideLoading()
+      }
+    })
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  requestUrl: requestUrl
 }
